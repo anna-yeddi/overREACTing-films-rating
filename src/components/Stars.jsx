f6a5082a339848faddfe7896bfeebffccb3e9506
Anna Kova <m3ta007@gmail.com>
2020-06-09 11:13:20 -0400

    Stars rating component is created

---
+++ src/components/Stars.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Star from './Star'
+
+function Stars(props) {
+  const count = props.count
+
+  return (
+    <>
+      <span className="sr-only">{count} Start Rating</span>
+      <ul className="card-body-stars u-clearfix" stars={count}>
+        {count >= 1 && count <= 5 && Array(count).fill(<Star />)}
+      </ul>
+    </>
+  )
+}
+
+// Set default value:
+Stars.defaultProps = {
+  props: { count: 0 },
+}
+
+Stars.propTypes = {
+  props: PropTypes.shape({
+    count: PropTypes.number,
+  }),
+}
+
+export default Stars
